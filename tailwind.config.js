4d15c1064afd290b343fc54819d243078cb4d2ab
claraa <140953929+SarahJessika1201@users.noreply.github.com>
2024-03-07 18:26:19 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./**/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                "back-dark": "#FFA966",
+                coffe: "#2F2105"
+            },
+        },
+    },
+    plugins: [],
+};
